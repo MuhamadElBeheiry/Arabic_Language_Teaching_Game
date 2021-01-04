48db3209c516d3c571cf379a7163af8241554e2c
eslamalaa10 <54743331+eslamalaa10@users.noreply.github.com>
2021-01-05 00:13:40 +0200

    add level 2

---
+++ Assets/scripts/home.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class home : MonoBehaviour
+{
+    public void next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void previouse()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void homebage()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
